174cdbca86da6bf967443903e36882f92969c9d7
John Sharratt <John@TheAlien.(none)>
2016-10-09 14:07:17 +0200

    Removed the spinlock from the main processing loop

--- kvblade.c
+++ kvblade.c
@@ -953,7 +953,7 @@ static __exit void kvblade_module_exit(void) {
     spin_unlock(&root.lock);
     
     for (; d; d = nd) {
-        nd = d->node.next;
+        nd = hlist_entry_safe(rcu_dereference_raw(hlist_next_rcu(&(pos)->member)), aoedev_t, node);
         while (atomic_read(&d->busy))
             msleep(100);
         
