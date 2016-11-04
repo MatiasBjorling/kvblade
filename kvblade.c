e45a88cd5e754ec23d823b85d7ac60f1bc872755
John Sharratt <John@TheAlien.(none)>
2016-11-04 22:44:28 +0100

    More cleaning up of some operations

--- kvblade.c
+++ kvblade.c
@@ -1055,11 +1055,13 @@ static struct sk_buff* clone_response(struct aoethread* t, struct sk_buff *skb, 
     if (skb_linearize(skb) < 0)
         return NULL;    
 
+    if (skb->len > skb->dev->mtu)
+        return NULL;
     rskb = skb_new(t, skb->dev, skb->dev->mtu);
     if (rskb == NULL)
         return NULL;
     
-    memcpy(skb_mac_header(rskb), skb_mac_header(skb), skb->len);
+    skb_copy_bits(skb, 0, skb_put(rskb, skb->len), skb->len);
     conv_response(t, rskb, major, minor);
     return rskb;
     
